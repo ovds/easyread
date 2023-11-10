44f46637a25a8d8163f30c9c635236bddecc28a1
iamdedeepsai <dedeepsai.vadapalli@gmail.com>
2023-11-11 02:49:34 +0800

    doc upload added

--- app/script/page.tsx
+++ app/script/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
-import {Button, ChakraProvider, Editable, EditablePreview, EditableTextarea, Textarea} from "@chakra-ui/react";
+import {Button, Center, ChakraProvider, IconButton, Textarea} from "@chakra-ui/react";
 import {useState} from "react";
 import {useRouter} from "next/navigation";
 import { CookiesProvider } from 'react-cookie';
+import Dropzone from "../components/dropzone";
 import 'regenerator-runtime/runtime'
+import {CloseIcon} from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
+
 
 export default function Page() {
     const [text, setText] = useState('');
@@ -20,6 +23,13 @@ export default function Page() {
         localStorage.setItem('text', text)
     }
 
+    const submitFile = (text: string) => {
+        router.push('/read');
+        localStorage.setItem('text', text)
+    }
+
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <CookiesProvider>
             <ChakraProvider>
@@ -30,7 +40,15 @@ export default function Page() {
                         </div>
                         <div className={'flex flex-row space-x-10 mt-5'}>
                             <Button onClick={submit}>Submit</Button>
-                            <Button colorScheme={'red'}>Upload Doc</Button>
+                            <Button colorScheme={'red'} onClick={() => setShowModal(true)}>Upload Doc</Button>
+                        </div>
+                    </div>
+                    <div className={"fixed inset-0 bg-black bg-opacity-50 z-10 w-screen h-screen " + (showModal ? "" : "hidden")}>
+                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-md shadow-lg w-1/2 h-1/4 lg:h-1/2">
+                            <Center className={'w-full h-full flex-col'}>
+                                <Dropzone onResult={submitFile} />
+                                <IconButton colorScheme={'red'} onClick={() => setShowModal(false)} aria-label={'close'} icon={<CloseIcon/>}></IconButton>
+                            </Center>
                         </div>
                     </div>
                 </div>
